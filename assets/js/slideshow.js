7bdc5c4002c54daaab7c5bd6a6afe90b93a3f58a
Amit Kumar Pal <amitsweb@gmail.com>
2026-08-23 01:26:15 +0530

    Create slideshow.js

---
+++ assets/js/slideshow.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  new Swiper(".hero-swiper", {
+
+    loop: true,
+
+    speed: 1200,
+
+    effect: "fade",
+
+    fadeEffect: {
+      crossFade: true
+    },
+
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true
+    },
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+
+  });
+
+});
